5eb94d107bb1b8c4274f33c93d6c85a9882a8dfa
Milad Nekofar <milad@nekofar.com>
2022-10-24 21:01:42 +0330

    feat: add new header, content and footer components

---
+++ components/content.tsx
@@ -0,0 +1,18 @@
+export default function Content() {
+  return (
+    <main className="mt-16 mx-auto max-w-7xl px-4 sm:mt-24">
+      <div className="text-center">
+        <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+          <span className="block xl:inline">Lil Nouns</span>{' '}
+          <span className="block text-neutral-600 xl:inline">Helpers</span>
+        </h1>
+        <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
+          Lil Nouns Helpers, aka Lil Helpers, are a group of volunteers; They are experienced builders and community
+          members that volunteer to facilitate new members&apos; access to information, building teams, proposals, and projects.
+        </p>
+        <div className="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
+        </div>
+      </div>
+    </main>
+  )
+}
