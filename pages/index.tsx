970017eefe2b5af3a8217ea282d913c9fac7a019
Milad Nekofar <milad@nekofar.com>
2023-02-09 22:26:18 +0330

    refactor: replace import alias on relative import paths

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Header from "../components/header";
-import Content from "../components/content";
-import Footer from "../components/footer";
+import Header from "@/components/header";
+import Content from "@/components/content";
+import Footer from "@/components/footer";
 
 const Home: NextPage = () => {
   return (
